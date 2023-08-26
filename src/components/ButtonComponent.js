563c1b6a23547cec6b8892a55cad08f8facca527
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-08-26 13:26:47 +0700

    adding profile and logout

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const Button = (props) => {
-    const { text } = props;
+    const { text, isLogout } = props;
 
     return (
-        <View style={styles.buttonContainer}>
+        <View style={
+            [
+                styles.buttonContainer,
+                {
+                    backgroundColor: isLogout ? '#F87B7B' : '#CAE3BB'
+                }
+            ]
+        }>
             <TouchableOpacity style={styles.button} {...props}>
                 <Text style={styles.text}>
                     {text}
@@ -21,8 +28,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderWidth: 1,
         borderRadius: 20,
-        margin: 16,
-        backgroundColor: '#CAE3BB'
+        margin: 16
     },
     button: {
         padding: 8
